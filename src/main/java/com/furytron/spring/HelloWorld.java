0c4f8423651980dce53eefd49f3cf211ae6815c0
Colin <goughc2@gmail.com>
2016-10-24 23:50:06 +0100

    reset HEAD

--- src/main/java/com/furytron/spring/HelloWorld.java
+++ src/main/java/com/furytron/spring/HelloWorld.java
@@ -8,5 +8,7 @@ public class HelloWorld {
 		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
+		
+		System.out.println("git checkout unstaging");
 	}
 }
